cfec8c70235fc6baca973731776d2e757a8b3c60
Luiz Guilherme <luizgmelo64@gmail.com>
2025-04-08 21:37:32 -0300

    docs: type of comments

--- src/academy/devdojo/javaoneforall/introduction/HelloDevDojo.java
+++ src/academy/devdojo/javaoneforall/introduction/HelloDevDojo.java
@@ -1,7 +1,11 @@
 package academy.devdojo.javaoneforall.introduction;
 
+/**
+ * This is a class HelloDevDojo and this is <b>javadoc</b>
+ */
 public class HelloDevDojo {
     public static void main(String[] args) {
+        // I can type anything and it will be completely ignored
         System.out.println("KA-ME-HA-ME-HAAA");
     }
 }
