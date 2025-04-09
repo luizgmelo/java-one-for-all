60591543cd1f86d103781be6a85926e5d6b9416d
Luiz Guilherme <luizgmelo64@gmail.com>
2025-04-08 22:14:13 -0300

    feat: add casting

--- src/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
+++ src/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -3,8 +3,9 @@ package academy.devdojo.javaoneforall.introduction;
 public class Class02PrimitiveTypes {
     public static void main(String[] args) {
         // int, double, float, boolean, char, byte, short, long
-        int age = 10;
-        long bigNumber = 10000;
+        // cast
+        int age = (int) 10000000000L;
+        long bigNumber = (long) 10000.00;
         float salaryFloat = 2500.0F;
         double salaryDouble = 2500.0;
         boolean trueValue = true;
@@ -13,5 +14,6 @@ public class Class02PrimitiveTypes {
         byte ageByte = -128;
         char character = 97;
         System.out.println("The age is " + age + " years old");
+        System.out.println(bigNumber);
     }
 }
