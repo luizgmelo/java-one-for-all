5c7c8da5700815741c0100b3d492be9f098f2243
Luiz Guilherme <luizgmelo64@gmail.com>
2025-04-08 22:02:16 -0300

    feat: add primitive types

--- src/academy/devdojo/javaoneforall/introduction/Class01HelloDevDojo.java
+++ src/academy/devdojo/javaoneforall/introduction/Class01HelloDevDojo.java
@@ -3,7 +3,7 @@ package academy.devdojo.javaoneforall.introduction;
 /**
  * This is a class HelloDevDojo and this is <b>javadoc</b>
  */
-public class HelloDevDojo {
+public class Class01HelloDevDojo {
     public static void main(String[] args) {
         // I can type anything and it will be completely ignored
         System.out.println("KA-ME-HA-ME-HAAA");
---
+++ src/academy/devdojo/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -0,0 +1,17 @@
+package academy.devdojo.javaoneforall.introduction;
+
+public class Class02PrimitiveTypes {
+    public static void main(String[] args) {
+        // int, double, float, boolean, char, byte, short, long
+        int age = 10;
+        long bigNumber = 10000;
+        float salaryFloat = 2500.0F;
+        double salaryDouble = 2500.0;
+        boolean trueValue = true;
+        boolean falseValue = false;
+        short ageShort = 29;
+        byte ageByte = -128;
+        char character = 97;
+        System.out.println("The age is " + age + " years old");
+    }
+}
