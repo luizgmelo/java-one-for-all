0eb2b41bd5fd8d9507d7d61f03d60644b0245d93
Luiz Guilherme <luizgmelo64@gmail.com>
2025-05-06 15:16:23 -0300

    feat(collections): work with hashCode

--- src/academy/devdojo/javaoneforall/javacore/Ycollections/domain/Smartphone.java
+++ src/academy/devdojo/javaoneforall/javacore/Ycollections/domain/Smartphone.java
@@ -24,6 +24,14 @@ public class Smartphone {
         return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
     }
 
+    // se x.equals(y) == true, y.hashCode() == x.hashCode()
+    // y.hashCode() == x.hashCode() não necessariamente o equals de y.equals(x) tem que ser true
+    // y.hashCode != x.hashCode() o x.equals(y) deverá ser false
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
