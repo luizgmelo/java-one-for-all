96db20ac2ae56fbece9d068738419152fb8da2df
Luiz Guilherme <luizgmelo64@gmail.com>
2025-05-11 09:46:23 -0300

    feat(CarPredicate): add function interface annotation

--- src/academy/devdojo/javaoneforall/javacore/ZZB/comportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/javaoneforall/javacore/ZZB/comportamento/interfaces/CarPredicate.java
@@ -2,6 +2,7 @@ package academy.devdojo.javaoneforall.javacore.ZZB.comportamento.interfaces;
 
 import academy.devdojo.javaoneforall.javacore.ZZB.comportamento.domain.Car;
 
+@FunctionalInterface
 public interface CarPredicate {
     boolean test(Car car);
 }
